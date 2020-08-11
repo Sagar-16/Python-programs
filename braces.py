75d489f9e312f72d6e6e7a2364adac5362c5bdd4
Sagar <sagar.dhampalwar1697@gmail.com>
2020-08-11 14:55:09 +0530

    output added

--- braces.py
+++ braces.py
@@ -1,18 +1,16 @@
+#python programs to generate balanced paranthesis
+
 def braces(open,close,op):
     if open == 0 and close == 0:
         l.append(op)
     if open !=0:
         op1 = op
-        op += '('
+        op1 += '('
         braces(open-1,close,op1)
     if close > open:
         op2 =op
         op2 += ')'
-        braces(open,close-1,op)
-
-
-
-
+        braces(open,close-1,op2)
 
 l=[]#list to store balanced paranthesis
 n=3 #take input from user
@@ -20,3 +18,16 @@ open = n
 close = n
 op ="" #to store output
 braces(open,close,op)
+
+
+for char in l:
+    print(char)
+
+
+#output:
+((()))
+(()())
+(())()
+()(())
+()()()
+
