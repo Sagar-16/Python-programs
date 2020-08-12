85d2865a085a4b78adc448ff728ae47d240874ad
Sagar <sagar.dhampalwar1697@gmail.com>
2020-08-12 17:25:50 +0530

    modified josephas program twice

--- josephas.py
+++ josephas.py
@@ -5,14 +5,14 @@ index=0 #starting person
 k=2 #kth person to kill
 k=k-1 # decreasing k as we starting from 0 location
 
-
-
+#function for killing kth person
 def solve(l,k,index):
     if(len(l)==1):
         return l[0]
     else:
-        index = (index+k)%len(l)
-        l.remove(l[index])
-        return solve(l,k,index)
+        index = (index+k)%len(l) #increating k % size for rotating in circu;lar fashion 
+        l.remove(l[index]) #removing kth person
+        return solve(l,k,index) #recursive call 
 x=solve(l,k,index)
-print(x)
+print(x) # printing op
+# answer here = 3
