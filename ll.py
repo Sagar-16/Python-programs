a414d5fd8e8ef0883a939401162314c6c41b0ac0
Sagar <sagar.dhampalwar1697@gmail.com>
2020-09-07 00:07:12 +0530

    second commit

--- ll.py
+++ ll.py
@@ -26,6 +26,8 @@ def TakeInput():
       tail.next=newNode
       tail = newNode
   return head
+
+
 def append(head,data):
   newNode = Node(data)
   tail= head
@@ -33,12 +35,41 @@ def append(head,data):
     tail= tail.next
   tail.next = newNode
   
+def insertAt(head,pos,value):
+  newNode = Node(value)
+  if pos<0 or pos >length(head):
+    return 
+  else:
+    tail= None
+    curr = head
+    count =0
+    while count<pos:
+      count +=1 
+      tail = curr
+      curr = curr.next
+    if tail is None:
+      newNode.next = curr
+      head = newNode
+    else:
+      tail.next = newNode
+      newNode.next = curr
 
 
+def length(head):
+  count =0
+  while head is not None:
+    count +=1
+    head = head.next
+  return count
+
+  
+def ithNode(head,pos):
+  for i in range(pos):
+    head = head.next
+  return (head.data)
 
 
 head = TakeInput()
 PrintList(head)
-append(head,10)
-append(head,20)
-PrintList(head)
+print(ithNode(head,1))
+print(ithNode(head,3))
