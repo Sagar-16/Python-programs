727f2fd21cdfd399470588d57eadfd2434494f67
Sagar <sagar.dhampalwar1697@gmail.com>
2020-09-07 12:16:03 +0530

    added sum.py file

---
+++ sum.py
@@ -0,0 +1,18 @@
+hash={}
+for i in range(1000):
+  hash[i]=0
+
+
+l=[1,2,3,4,5,6,7,9,10]
+sum =8
+
+
+def findPair(l,sum):
+  for i in range(len(l)):
+    temp = sum - l[i]
+    if(temp in hash):
+      print(f"{l[i]} {temp}")
+    else:
+      hash[l[i]]=1
+
+findPair(l,sum)
