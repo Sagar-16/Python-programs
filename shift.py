8d508c50eefbcef1eb905611531ecece87a8ab90
Sagar <sagar.dhampalwar1697@gmail.com>
2020-09-08 21:13:29 +0530

    shift.py added

---
+++ shift.py
@@ -0,0 +1,25 @@
+def shift(numbers,number):
+  i=0
+  j=0
+  while i<len(numbers):
+    if(numbers[i]!=number):
+      numbers[j]=numbers[i]
+      i+=1
+      j+=1
+    else:
+      i+=1
+  while j<len(numbers):
+    numbers[j]=number
+    j +=1
+  return numbers
+
+
+def main():
+  numbers =[int(x) for x in input().split()]
+  number = int(input())
+  print(shift(numbers,number))
+
+main()
+# [3,2,2,2,1,4,2,2] 2
+
+
